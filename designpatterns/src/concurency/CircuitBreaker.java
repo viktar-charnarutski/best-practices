f9370e84b3dce8b79862f5c56122ae621c36b435
viktar_charnarutski <v.charnarutski@gmail.com>
2017-06-12 12:15:43 -0700

    Circuit breaker design pattern implementation. Adding some java doc.

--- designpatterns/src/concurency/CircuitBreaker.java
+++ designpatterns/src/concurency/CircuitBreaker.java
@@ -5,11 +5,23 @@ package concurency;
  */
 public interface CircuitBreaker {
 
+    /**
+     * Opens the <em>Circuit Breaker</em>.
+     */
     void openCircuitBreaker();
 
+    /**
+     * Closes the <em>Circuit Breaker</em>.
+     */
     void closeCircuitBreaker();
 
+    /**
+     * Checks if the <em>Circuit Breaker</em> is in <em>open</em> state.
+     */
     boolean isCircuitBreakerOpen();
 
+    /**
+     * Checks if the <em>Circuit Breaker</em> is in <em>closed</em> state.
+     */
     boolean isCircuitBreakerClosed();
 }
